7ab4b88e33887367e27df9780648f19f2287693b
Vitaliy <theyamshikov@yandex.ru>
2017-12-18 13:00:49 +0600

    pretty code

--- src/drivers/Json.php
+++ src/drivers/Json.php
@@ -15,7 +15,8 @@ class Json implements DriverInterface
     }
 
     public function encode($data) {
-        $content = json_encode($data, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
+        $content = json_encode($data, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+	    $content = str_replace('    ', "\t", $content);
         return $content;
     }
 
